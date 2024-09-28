441de3de86c15669b35c3cc7de17a2e5e7cdd5c8
homemix <kenmutati@gmail.com>
2024-09-28 11:34:03 +0300

    Add: Creating home page.

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Author, Book, BookInstance, Genre
+
+
+def index(request):
+    """View Function for home page of site"""
+
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books
+    num_instances_available = BookInstance.objects.all().filter(status__exact='a').count()
+
+    num_authors = Author.objects.count()
+    num_genres = Genre.objects.count()
+    num_books_with_soldier = Book.objects.filter(summary__icontains='soldier').count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_books_with_soldier': num_books_with_soldier,
+    }
+
+    return render(request, template_name='index.html', context=context)
--- local_library/settings.py
+++ local_library/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "catalog.apps.CatalogConfig",
-    'django_seed',
+    # 'django_seed',
 ]
 
 MIDDLEWARE = [
