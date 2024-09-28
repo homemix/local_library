06160da1ba509c40d173aee1ba632afcefa8710c
homemix <kenmutati@gmail.com>
2024-09-28 13:08:40 +0300

    Add: Generic list and detail views.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
 from catalog import views
+from catalog.views import BookListView,BookDetailView,AuthorListView,AuthorDetailView
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/',BookListView.as_view(), name='books'),
+    path('authors/',AuthorListView.as_view(), name='authors'),
+    path('books/<int:pk>',BookDetailView.as_view(), name='book-detail'),
+    path('authors/<int:pk>',AuthorDetailView.as_view(), name='author-detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from .models import Author, Book, BookInstance, Genre
 
@@ -26,3 +27,32 @@ def index(request):
     }
 
     return render(request, template_name='index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'book_list'
+    paginate_by = 5
+
+    def get_queryset(self):
+        return Book.objects.all()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+
+        context['some_data'] = 'test data'
+        return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 5
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
